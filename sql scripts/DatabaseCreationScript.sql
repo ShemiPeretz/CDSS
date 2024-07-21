321a13c99cc40fd552fa96c7544cac048c010a5e
Shimon Peretz <shimon@uniqai.com>
2024-07-21 11:13:12 +0300

    sql scripts changes

--- sql scripts/DatabaseCreationScript.sql
+++ sql scripts/DatabaseCreationScript.sql
@@ -4,36 +4,45 @@ USE clinical_dss;
 
 CREATE TABLE patients (
     patient_id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(100),
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
     gender ENUM('Male', 'Female')
+);
+
+CREATE TABLE tests_to_lionc_num (
+	test_type TEXT,
+    LIONC_NUM TEXT
 );
 
 CREATE TABLE tests (
     test_id INT PRIMARY KEY AUTO_INCREMENT,
     patient_id INT,
-    test_date DATE,
+    transaction_time DATE,
+    valid_start_time DATE,
+    valid_end_time DATE,
+    LOINC_NUM TEXT,
     hemoglobin_level FLOAT,
     wbc_level FLOAT,
-    fever BOOLEAN,
-    chills BOOLEAN,
-    skin_look ENUM('Normal', 'Pale', 'Flushed', 'Jaundiced'),
-    allergic_state ENUM('None', 'Mild', 'Moderate', 'Severe'),
+    fever FLOAT,
+    chills ENUM("None", "Rigor", "Shaking"),
+    skin_look ENUM("Erythema", "Vesiculation", "Desquamation", "Exfoliation"),
+    allergic_state ENUM("Edema", "Bronchospasm", "Severe-Bronchospasm", "Anaphylactic-Shock"),
     FOREIGN KEY (patient_id) REFERENCES patients(patient_id)
 );
 
 CREATE TABLE patient_states (
     state_id INT PRIMARY KEY AUTO_INCREMENT,
     test_id INT,
-    hemoglobin_state ENUM('Low', 'Normal', 'High'),
-    hematological_state ENUM('Normal', 'Abnormal'),
-    systematic_toxicity ENUM('None', 'Mild', 'Moderate', 'Severe'),
+    hemoglobin_state ENUM("Severe-Anemia", "Moderate-Anemia", "Mild-Anemia", "Normal-Hemoglobin", "Polyhemia", "Polycytemia"),
+    hematological_state ENUM("Normal", "Polyhemia", "Pancytopenia", "Anemia", "Suspected-Leukemia", "Leukemoid-reaction", "Suspected-Polycytemia-Vera", "Leukopenia"),
+    systemic_toxicity ENUM("Grade 1", "Grade 2", "Grade 3", "Grade 4"),
     FOREIGN KEY (test_id) REFERENCES tests(test_id)
 );
 
 CREATE TABLE treatments (
     treatment_id INT PRIMARY KEY AUTO_INCREMENT,
-    hemoglobin_state ENUM('Low', 'Normal', 'High'),
-    hematological_state ENUM('Normal', 'Abnormal'),
-    systematic_toxicity ENUM('None', 'Mild', 'Moderate', 'Severe'),
+    hemoglobin_state ENUM("Severe-Anemia", "Moderate-Anemia", "Mild-Anemia", "Normal-Hemoglobin", "Polyhemia", "Polycytemia"),
+    hematological_state ENUM("Normal", "Polyhemia", "Pancytopenia", "Anemia", "Suspected-Leukemia", "Leukemoid-reaction", "Suspected-Polycytemia-Vera", "Leukopenia"),
+    systemic_toxicity ENUM("Grade 1", "Grade 2", "Grade 3", "Grade 4"),
     recommended_treatment TEXT
 );
